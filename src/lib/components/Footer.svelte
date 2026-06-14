90651710848400c109c915405b1a69f562dd47f0
joaomidowz <jgcustodio2005@gmail.com>
2026-06-14 13:42:09 -0300

    fix: update contact email to cs13a0

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -20,8 +20,7 @@
       <a class="support-link" href="https://ko-fi.com/cs13a0" target="_blank" rel="noopener noreferrer">
         {labels.support}
       </a>
-      <!-- TODO: trocar para contato@cs13a0.com quando a nova caixa postal estiver ativa. -->
-      <a href="mailto:contato@cs20a0.com"><span>{labels.contact}</span> contato@cs20a0.com</a>
+      <a href="mailto:contato@cs13a0.com"><span>{labels.contact}</span> contato@cs13a0.com</a>
     </div>
 
     <p class="footer-disclaimer">{labels.disclaimer}</p>
